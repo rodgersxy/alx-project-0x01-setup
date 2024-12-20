0a1face8d76665ee2580f4671d1f221d8fcc0d16
rodgersxy <rodgersny99@gmail.com>
2024-12-20 13:20:01 +0300

    add: More Components (Extending Post Page)

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -4,28 +4,28 @@ export interface PostProps {
     id: number;
     title: string;
     body: string;
-  }
+}
   
-  export interface GeoProps {
+export interface GeoProps {
     lat: string;
     lng: string;
-  }
+}
   
-  export interface AddressProps {
+export interface AddressProps {
     street: string;
     suite: string;
     city: string;
     zipcode: string;
     geo: GeoProps;
-  }
+}
   
-  export interface CompanyProps {
+export interface CompanyProps {
     name: string;
     catchPhrase: string;
     bs: string;
-  }
+}
   
-  export interface UserProps {
+export interface UserProps {
     id: number;
     name: string;
     username: string;
@@ -34,4 +34,16 @@ export interface PostProps {
     phone: string;
     website: string;
     company: CompanyProps;
-  }
+}
+
+
+export interface PostData {
+    userId: number;
+    id?: number;
+    title: string;
+    body: string;
+}
+export interface PostModalProps {
+    onClose: () => void;
+    onSubmit: (post: PostData) => void;
+}
